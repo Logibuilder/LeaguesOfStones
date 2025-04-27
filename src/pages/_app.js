195d4d27f3232704cb4efe6075f934410860d821
Logibuilder <161773002+Logibuilder@users.noreply.github.com>
2025-04-28 02:16:10 +0300

    Delete src directory

--- src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "@/styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
